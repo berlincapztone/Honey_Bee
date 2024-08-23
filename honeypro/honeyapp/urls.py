613823abe5540047d57632e815aa1cc25b85a038
berlin joe d <berlin@capztone.com>
2024-08-23 12:36:44 +0530

    comitt

--- honeypro/honeyapp/urls.py
+++ honeypro/honeyapp/urls.py
@@ -3,6 +3,6 @@ from . import views
 
 urlpatterns = [
     path('', views.landing_page, name='landing_page'),
-  
+    path('oil/', views.oil_page, name='oil_page'),
      
 ]
--- honeypro/honeyapp/views.py
+++ honeypro/honeyapp/views.py
@@ -2,3 +2,6 @@ from django.shortcuts import render
 
 def landing_page(request):
     return render(request, 'index.html')
+
+def oil_page(request):
+    return render(request, 'oil.html')
