d74d5e09921c0cb3057dac960d9e17f2320af4f1
berlin joe d <berlin@capztone.com>
2024-08-27 10:51:09 +0530

    commit all changes

--- honeypro/honeypro/settings.py
+++ honeypro/honeypro/settings.py
@@ -80,12 +80,10 @@ TIME_ZONE = 'UTC'
 USE_I18N = True
 USE_TZ = True
 
-# Static files (CSS, JavaScript, Images)
 STATIC_URL = '/static/'
-
-# Directories for collecting static files
 STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+
 
 # Default primary key field type
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
